dc3822afa1670bcee15439cc50e14af99a45e9bb
aqquejado <aqquejado@jlabs.team>
2024-01-31 14:48:18 +0800

    Forgot password feature

---
+++ app/Http/Controllers/PasswordController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Validator;
+
+class PasswordController extends Controller
+{
+    public function send(Request $request) {
+        $validator = Validator::make($request->only(["email","password"]), [
+            "email" => ["required","string","email", Rule::exists("users", "email"), "max:255"]
+        ]);
+
+        if ($validator->fails()) {
+            return response(["message"=> "Bad request", "errors" => $validator->errors()], 400);
+        }
+ 
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+    
+        return $status === Password::RESET_LINK_SENT
+            ? response(['message' => __($status)], 200)
+            : response(['status' => __($status)], 500);
+    }
+
+    public function redirectReset(string $token) {
+        return redirect()->to(env("CLIENT_APP_URL") . "/reset-password?token=" . $token);
+    }
+
+    public function reset(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "email" => ["required","string","email", Rule::exists("users", "email"), "max:255"],
+            "password" => "required|string|min:8|max:255|confirmed",
+            "token" => "required"
+        ]);
+
+        if ($validator->fails()) {
+            return response(["message"=> "Bad request", "errors" => $validator->errors()], 400);
+        }
+ 
+        $status = Password::reset(
+            $request->only("email", "password", "password_confirmation", "token"),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+    
+        return $status === Password::PASSWORD_RESET
+            ? response(["message" => __($status)], 200)
+            : response(['status' => __($status)], 500);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\VerificationController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,16 +16,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-// TODO: validator
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::get('/email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
 
-Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+Route::get('/email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+
+Route::post('/forgot-password', [PasswordController::class, 'send']);
+
+Route::get('/reset-password/{token}', [PasswordController::class, 'redirectReset'])->name('password.reset');
+
+Route::post('/reset-password', [PasswordController::class, 'reset'])->name('password.update');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
